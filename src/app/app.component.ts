eedad9b00e28f5d6548cd24f81813887debba670
Prabhjeet Singh <W0849306@myscc.ca>
2024-09-17 23:56:05 -0400

    *Add an array of 6 instances *Add Html to page. *Add the css.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {DataType} from "./DataTypeInterface/user";
+import {DatePipe, NgForOf, NgIf} from "@angular/common";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DatePipe, NgForOf, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
+
   title = 'Prabhjeet-Singh-Learning-Angular';
   fname = 'Prabhjeet'
   lname= 'Singh'
+  dataArray: DataType[] = [
+    { id: 1, name: 'Item 1', description: 'Description 1', Date: new Date(), quantity: 1},
+    { id: 2, name: 'Item 2,', Date: new Date(), quantity: 2},
+    { id: 3, name: 'Item 3', description: 'Description 3', Date: new Date(), quantity: 2},
+    { id: 4, name: 'Item 4', description: 'Description 4', Date: new Date(), quantity: 3},
+    { id: 5, name: 'Item 5', Date: new Date(), quantity: 2},
+    { id: 6, name: 'Item 6', description: 'Description 6', Date: new Date(), quantity: 6}
+  ];
 }
+
