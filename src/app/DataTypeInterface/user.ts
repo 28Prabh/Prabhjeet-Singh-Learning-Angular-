7ce0b8e9b6f10d13e50af4326b51d7ae0c561aaa
Prabhjeet Singh <W0849306@myscc.ca>
2024-09-26 17:41:30 -0400

    *Add an array of 6 instances *Add Html to page. *Add the css. *Forgot to do unversioned Files

---
+++ src/app/DataTypeInterface/user.ts
@@ -0,0 +1,9 @@
+// Directory of this file src/app/DataTypeInterface.user.ts
+
+export interface DataType {
+  id: number;
+  name: string;
+  description?: string; // Optional property
+  Date: Date;
+  quantity: number;
+}
