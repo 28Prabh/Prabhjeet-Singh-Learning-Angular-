007d944021ae74633b7edfc57c0528ddab7992ca
Prabhjeet Singh <W0849306@myscc.ca>
2024-09-27 14:52:49 -0400

    *Created two UI's *One for list and other for ListItems

---
+++ src/app/shoe-list-item/shoe-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shoe-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './shoe-list-item.component.html',
+  styleUrl: './shoe-list-item.component.css'
+})
+export class ShoeListItemComponent {
+
+}
