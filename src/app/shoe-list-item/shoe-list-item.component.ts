d70e22d910baaa154a75048b45d69594ddcae557
Prabhjeet Singh <W0849306@myscc.ca>
2024-09-27 16:04:40 -0400

    Did ALL THE ESSIENTIAL steps

--- src/app/shoe-list-item/shoe-list-item.component.ts
+++ src/app/shoe-list-item/shoe-list-item.component.ts
@@ -1,12 +1,16 @@
 import {Component, Input} from '@angular/core';
+import {NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-shoe-list-item',
   standalone: true,
-  imports: [],
+  imports: [
+    NgStyle
+  ],
   templateUrl: './shoe-list-item.component.html',
   styleUrl: './shoe-list-item.component.css'
 })
 export class ShoeListItemComponent {
 @Input() shoe?: any;
+  @Input() index!: number;
 }
