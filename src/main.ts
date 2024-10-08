a0882bd5082f5c5660035912324c7d1ab26cda46
Prabhjeet Singh <W0849306@myscc.ca>
2024-10-08 17:58:58 -0400

    Completed all the steps until7

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from "@angular/router";
+import {ShoeListComponent} from "./app/shoe-list/shoe-list.component";
+import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
+import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+
+
+
+const routes: Routes =[{ path: 'shoes', component: ShoeListComponent },
+  {path:'modify-student', component: ModifyListItemComponent},
+  {path: '**', component:PageNotFoundComponent}];
+bootstrapApplication(AppComponent, {providers: [provideRouter(routes)]})
+  .then(r => console.log('Bootstrap successful'));
