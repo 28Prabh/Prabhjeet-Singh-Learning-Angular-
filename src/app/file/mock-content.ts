24201a0dcb27f24c8544a5a5759a86d3de0e4915
Prabhjeet Singh <W0849306@myscc.ca>
2024-11-14 13:26:09 -0500

    Resolved the date issue

--- src/app/file/mock-content.ts
+++ src/app/file/mock-content.ts
@@ -1,8 +1,8 @@
 import {DataType} from "../DataTypeInterface/shoe";
 
 export let shoeList: DataType[] = [
-  { id: 1, name: 'NikeJordan1', description: 'These ones are high ancle', Date: "2024-6-01", quantity: 1},
+  { id: 1, name: 'NikeJordan1', description: 'These ones are high ancle', Date: "2024-06-01", quantity: 1},
   { id: 2, name: 'NikeAirforce', description:'These are till ancles', Date: "2023-04-01", quantity: 2},
   { id: 3, name: 'NikeDunks', description: 'these have big nike logo on them', Date: "2024-06-11", quantity: 2},
-  { id: 4, name: 'Puma', description: 'Its a competetor of Nike', Date: "2024-8-02", quantity: 3},
+  { id: 4, name: 'Puma', description: 'Its a competetor of Nike', Date: "2024-08-02", quantity: 3},
 ];
