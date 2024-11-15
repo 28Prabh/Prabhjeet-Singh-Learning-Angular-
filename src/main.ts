6afeaa8fab8c2d98cfd88c4445c4446299c43164
Prabhjeet Singh <W0849306@myscc.ca>
2024-11-15 16:05:36 -0500

    Added Lazy Loading

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import {provideRouter, Routes} from "@angular/router";
 import {ShoeListComponent} from "./app/shoe-list/shoe-list.component";
-import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
-import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
-import {FormComponent} from "./app/form/form.component";
 
 
 
@@ -13,10 +10,14 @@ import {FormComponent} from "./app/form/form.component";
 
 const routes: Routes =[  {path:'', redirectTo: '/shoes', pathMatch: 'full'},
   { path: 'shoes', component: ShoeListComponent },
-  {path:'modify-student', component: ModifyListItemComponent},
-  {path: 'form', component:FormComponent},
-  {path: 'modify-student/:id', component:ModifyListItemComponent},
-  {path: '**', component:PageNotFoundComponent}
+  {path:'modify-student', loadComponent: () =>
+      import("./app/modify-list-item/modify-list-item.component").then(m => m.ModifyListItemComponent)},
+  {path: 'form', loadComponent: () =>
+      import("./app/form/form.component").then(m => m.FormComponent)},
+  {path: 'modify-student/:id',loadComponent: () =>
+      import("./app/modify-list-item/modify-list-item.component").then(m => m.ModifyListItemComponent)},
+  {path: '**', loadComponent: () =>
+      import("./app/page-not-found/page-not-found.component").then(m => m.PageNotFoundComponent)}
 ];
 bootstrapApplication(AppComponent, {providers: [provideRouter(routes)]})
   .then(r => console.log('Bootstrap successful'));
